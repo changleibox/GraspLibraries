eacb6864f594990f3293972d721ebe0cd4f26eff
changlei <changlei@graspyun.com>
2021-02-25 13:38:36 +0800

    增加通用依赖库

--- baselibrary/src/main/java/me/box/plugin/baselibrary/retrofit/HttpRequestWrapper.java
+++ baselibrary/src/main/java/me/box/plugin/baselibrary/retrofit/HttpRequestWrapper.java
@@ -29,25 +29,25 @@ public class HttpRequestWrapper<E> {
     private final HttpRequest mRequest;
     private final E mService;
 
-    protected HttpRequestWrapper(String baseUrl, long timeout, Class<E> cls, Interceptor... interceptors) {
+    public HttpRequestWrapper(String baseUrl, long timeout, Class<E> cls, Interceptor... interceptors) {
         HttpRequest.initialize("RetCode", "RetMessage", "RetObject", 0);
         mRequest = new HttpRequest(baseUrl, timeout, HttpRequestWrapper::log, interceptors);
         mService = mRequest.create(cls);
     }
 
-    protected <T> Subscriber<T> request(@Nullable RetrofitContext iContext, @NonNull Observable<T> observable, @Nullable Action1<T> observer) {
+    public <T> Subscriber<T> request(@Nullable RetrofitContext iContext, @NonNull Observable<T> observable, @Nullable Action1<T> observer) {
         return request(iContext, observable, observer, true);
     }
 
-    protected <T> Subscriber<T> request(@Nullable RetrofitContext iContext, @NonNull Observable<T> observable, @Nullable Action1<T> observer, boolean isShowPrompt) {
+    public <T> Subscriber<T> request(@Nullable RetrofitContext iContext, @NonNull Observable<T> observable, @Nullable Action1<T> observer, boolean isShowPrompt) {
         return request(iContext, observable, Callback.inclusion(observer), isShowPrompt);
     }
 
-    protected <T> Subscriber<T> request(@Nullable RetrofitContext iContext, @NonNull Observable<T> observable, @Nullable Observer<T> observer) {
+    public <T> Subscriber<T> request(@Nullable RetrofitContext iContext, @NonNull Observable<T> observable, @Nullable Observer<T> observer) {
         return request(iContext, observable, observer, true);
     }
 
-    protected <T> Subscriber<T> request(@Nullable RetrofitContext iContext, @NonNull Observable<T> observable, @Nullable Observer<T> observer, boolean isShowPrompt) {
+    public <T> Subscriber<T> request(@Nullable RetrofitContext iContext, @NonNull Observable<T> observable, @Nullable Observer<T> observer, boolean isShowPrompt) {
         return mRequest.request(iContext, observable, new SubscriberWrapper<>(observer, isShowPrompt));
     }
 
@@ -55,11 +55,11 @@ public class HttpRequestWrapper<E> {
         Log.i("HttpRequest", s);
     }
 
-    protected HttpRequest getRequest() {
+    public HttpRequest getRequest() {
         return mRequest;
     }
 
-    protected E getService() {
+    public E getService() {
         return mService;
     }
 }
